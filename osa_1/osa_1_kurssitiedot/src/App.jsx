a83a9383c3702dc7145b08a6c5d73fb066fe9157
Suvi Liimatainen <suvi.liimatainen@gmail.com>
2026-09-04 18:28:02 +0200

    exercise 1.3 completed

--- osa_1/osa_1_kurssitiedot/src/App.jsx
+++ osa_1/osa_1_kurssitiedot/src/App.jsx
@@ -24,17 +24,20 @@
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-  const parts = [
-    { name: part1, exercises: exercises1 },
-    { name: part2, exercises: exercises2 },
-    { name: part3, exercises: exercises3 }
-  ]
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+
+  const parts = [part1, part2, part3]
 
 
   return (
@@ -45,5 +48,30 @@ const App = () => {
     </div>
   )
 }
+
+// const App = () => {
+//   const course = 'Half Stack application development'
+//   const part1 = 'Fundamentals of React'
+//   const exercises1 = 10
+//   const part2 = 'Using props to pass data'
+//   const exercises2 = 7
+//   const part3 = 'State of a component'
+//   const exercises3 = 14
+//   const parts = [
+//     { name: part1, exercises: exercises1 },
+//     { name: part2, exercises: exercises2 },
+//     { name: part3, exercises: exercises3 }
+//   ]
+
+
+//   return (
+//     <div>
+//       <Header course={course} />
+//       <Content parts={parts} />
+//       <Total parts={parts} />
+//     </div>
+//   )
+// }
+
 
 export default App
