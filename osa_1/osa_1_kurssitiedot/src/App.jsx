c28a9c06ca55b54db7c8489c63f0fcd0250f5388
Suvi Liimatainen <suvi.liimatainen@gmail.com>
2026-09-04 17:53:40 +0200

    exercises 1.1 completed

---
+++ osa_1/osa_1_kurssitiedot/src/App.jsx
@@ -0,0 +1,45 @@
+
+  const Header = ({ course }) => {
+    return <h1>{course}</h1>
+  }
+
+  const Content = ({ parts }) => {
+    return (
+      <div>
+        <p>{parts[0].name} {parts[0].exercises}</p>
+        <p>{parts[1].name} {parts[1].exercises}</p>
+        <p>{parts[2].name} {parts[2].exercises}</p> 
+      </div>
+    )
+  }
+
+  const Total = ({ parts }) => {
+    const total = parts[0].exercises + parts[1].exercises + parts[2].exercises
+    return <p>Number of exercises {total}</p>
+  }
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+  const parts = [
+    { name: part1, exercises: exercises1 },
+    { name: part2, exercises: exercises2 },
+    { name: part3, exercises: exercises3 }
+  ]
+
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total parts={parts} />
+    </div>
+  )
+}
+
+export default App
