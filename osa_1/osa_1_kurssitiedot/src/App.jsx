f74e13d3dbadffcf50df1c25cf805906b8d07c69
Suvi Liimatainen <suvi.liimatainen@gmail.com>
2026-09-08 17:51:29 +0200

    exercise 1.4 completed

--- osa_1/osa_1_kurssitiedot/src/App.jsx
+++ osa_1/osa_1_kurssitiedot/src/App.jsx
@@ -2,17 +2,16 @@
   const Header = ({ course }) => {
     return <h1>{course}</h1>
   }
-
-  const Part = ({ part }) => {
-    return <p>{part.name} {part.exercises}</p>
-  }
+// This component is now not really useful because we created the partslist so I removed it.
+  // const Part = ({ part }) => {
+  //   return <p>{part.name} {part.exercises}</p>
+  // }
 
   const Content = ({ parts }) => {
+    const contentParts = parts.map(part => <p>{part.name} {part.exercises}</p>)
     return (
       <div>
-        <Part part={parts[0]} />
-        <Part part={parts[1]} />
-        <Part part={parts[2]} /> 
+        {contentParts}
       </div>
     )
   }
@@ -24,54 +23,30 @@
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
+  const parts_list = [{
     name: 'Fundamentals of React',
     exercises: 10
-  }
-  const part2 = {
+  },
+  {
     name: 'Using props to pass data',
     exercises: 7
-  }
-  const part3 = {
+  },
+  {
     name: 'State of a component',
     exercises: 14
-  }
+  }]
 
-  const parts = [part1, part2, part3]
 
 
   return (
     <div>
       <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Content parts={parts_list} />
+      <Total parts={parts_list} />
     </div>
   )
 }
 
-// const App = () => {
-//   const course = 'Half Stack application development'
-//   const part1 = 'Fundamentals of React'
-//   const exercises1 = 10
-//   const part2 = 'Using props to pass data'
-//   const exercises2 = 7
-//   const part3 = 'State of a component'
-//   const exercises3 = 14
-//   const parts = [
-//     { name: part1, exercises: exercises1 },
-//     { name: part2, exercises: exercises2 },
-//     { name: part3, exercises: exercises3 }
-//   ]
-
-
-//   return (
-//     <div>
-//       <Header course={course} />
-//       <Content parts={parts} />
-//       <Total parts={parts} />
-//     </div>
-//   )
-// }
 
 
 export default App
