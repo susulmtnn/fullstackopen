1d0848d15f42cdd8d2b091e009c728f2ae55ce9e
Suvi Liimatainen <suvi.liimatainen@gmail.com>
2026-09-08 18:01:13 +0200

    completed exercise 1.5

--- osa_1/osa_1_kurssitiedot/src/App.jsx
+++ osa_1/osa_1_kurssitiedot/src/App.jsx
@@ -1,6 +1,6 @@
 
   const Header = ({ course }) => {
-    return <h1>{course}</h1>
+    return <h1>{course.name}</h1>
   }
 // This component is now not really useful because we created the partslist so I removed it.
   // const Part = ({ part }) => {
@@ -22,27 +22,43 @@
   }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts_list = [{
-    name: 'Fundamentals of React',
-    exercises: 10
-  },
-  {
-    name: 'Using props to pass data',
-    exercises: 7
-  },
-  {
-    name: 'State of a component',
-    exercises: 14
-  }]
-
+  // const course = 'Half Stack application development'
+  // const parts_list = [{
+  //   name: 'Fundamentals of React',
+  //   exercises: 10
+  // },
+  // {
+  //   name: 'Using props to pass data',
+  //   exercises: 7
+  // },
+  // {
+  //   name: 'State of a component',
+  //   exercises: 14
+  // }]
+const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
 
   return (
     <div>
       <Header course={course} />
-      <Content parts={parts_list} />
-      <Total parts={parts_list} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
