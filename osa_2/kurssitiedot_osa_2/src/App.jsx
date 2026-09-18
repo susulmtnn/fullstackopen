8cf49a86ac8a0ab6d6535f2cffdb4307d165af25
Suvi Liimatainen <suvi.liimatainen@gmail.com>
2026-09-18 21:56:40 +0200

    completed 2.1

---
+++ osa_2/kurssitiedot_osa_2/src/App.jsx
@@ -0,0 +1,58 @@
+
+const Header = (props) => <h1>{props.course}</h1>
+
+const Content = (props) => (
+  <div>
+    <Part part={props.parts[0]} />
+    <Part part={props.parts[1]} />
+    <Part part={props.parts[2]} />
+  </div>
+)
+
+const Part = (props) => (
+  <p>
+    {props.part.name} {props.part.exercises}
+  </p>
+)
+
+const Total = (props) => <p>Number of exercises {props.total}</p>
+
+const Course = (props) => (
+  <div>
+    <Header course={props.course.name} />
+    <Content parts={props.course.parts} />
+    <Total total={props.course.parts.reduce((sum, part) => sum + part.exercises, 0)} />
+  </div>
+)
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    id: 1,
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Course course={course} />
+    </div>
+  )
+}
+
+export default App
